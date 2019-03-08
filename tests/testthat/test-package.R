580c6f31118ccf89b6da91f47ae664d1512a8bcd
Jonathan Sidi <yonis@metrumrg.com>
2019-03-08 09:34:31 -0500

    replace devtools with rcmdcheck in silent_check

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -8,6 +8,6 @@ testthat::test_that('package',{
   x <- tidycheckUsage::tidycheckUsagePackage(pack = 'tidycheckUsage')
   
   expect_s3_class(x,'data.frame')
-  expect_equal(nrow(x),2)
+  expect_equal(nrow(x),3)
 
 })
